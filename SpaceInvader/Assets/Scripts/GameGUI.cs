536adfcca9c49aecddbbbd27b391ea3f6f07f5ce
LyzelPrado <l.prado@klab.com>
2013-09-17 15:12:01 +0800

    Display player score and lives in the game scene using NGUI

---
+++ SpaceInvader/Assets/Scripts/GameGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		UILabel score = GameObject.Find("NumScore").GetComponent<UILabel>();
+		score.text = Player.score.ToString();
+		
+		UILabel lives = GameObject.Find("NumLives").GetComponent<UILabel>();
+		lives.text = Player.playerLives.ToString();
+	
+	}
+}
