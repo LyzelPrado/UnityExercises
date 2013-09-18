887b55257f37619fa1bf98f4babf23e1455f6e02
LyzelPrado <l.prado@klab.com>
2013-09-18 12:04:04 +0800

    Created game Over Menu

---
+++ SpaceInvader/Assets/Scripts/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+	
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+		UILabel score = GameObject.Find("FinalScore").GetComponent<UILabel>();
+		score.text = Player.score.ToString();
+		StartCoroutine(BackToMain());
+	
+		
+	}
+	
+	IEnumerator BackToMain()
+	{
+	    yield return new WaitForSeconds(3.0f);
+	    Application.LoadLevel(0);
+		Player.score = 0;
+		Player.playerLives = 3;
+	}
+
+	
+}
