8c7becfc85655dc54c7f5ebfe4ebcf05b0f3c4d4
Lyzel Prado <lyzelprado@MAC011.local>
2013-09-19 16:54:08 +0800

    Modify player control for IOS

--- SpaceInvader/Assets/Scripts/Player.cs
+++ SpaceInvader/Assets/Scripts/Player.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public float playerSpeed = 10.0f;
+	public float playerSpeed = 1000.0f;
 	public static int playerLives=3;
 	public static int score = 0;
 	public static float timer = 0f;
+	bool isTouch = true;
 
 	// reference prefabs
 	public GameObject Lazer;
-	//GameObject Enemy;
-
+	
 
 	// Use this for initialization
 	void Start () {
-		//spawn point
-		// position the player
+		
+	
 		transform.position = new Vector3(0, 0, 0);
 
 
@@ -26,48 +26,67 @@ public class Player : MonoBehaviour {
 	void Update () {
 
 		//move the player left to right
-		transform.Translate(Vector3.right * playerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		
+		Vector3 dir = Vector3.zero;
+		dir.x = Input.acceleration.x;
+		dir.y = 0;
+		dir.z = 0;
+			
+		if (dir.sqrMagnitude > 1)
+				dir.Normalize();
+		
+		dir *= Time.deltaTime;
+		transform.Translate(dir*playerSpeed);
 
+		// if the player object is positioned at x=15 then it will appear at x= -15 and vice versa
 
-		// if the player object is positioned at x=18 then it will appear at x= -18 and vice versa
+		if(transform.position.x > 15)
+			transform.position = new Vector3(-15,transform.position.y, transform.position.z );
 
-		if(transform.position.x > 18)
-			transform.position = new Vector3(-18,transform.position.y, transform.position.z );
+		else if (transform.position.x < -15)
+			transform.position = new Vector3(15,transform.position.y, transform.position.z );
 
-		else if (transform.position.x < -18)
-			transform.position = new Vector3(18,transform.position.y, transform.position.z );
-
-		//press space bar to fire a laser
-
-		if (Input.GetKeyDown(KeyCode.Space))
+		//touch screen to fire a laser
+		
+		if(isTouch)
 		{
-			Vector3 projectPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-			Instantiate(Lazer, projectPos, Quaternion.identity);
-
+			foreach(Touch touch in Input.touches)
+			{
+				if (touch.phase == TouchPhase.Began)
+				{
+					Vector3 projectPos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+					Instantiate(Lazer, projectPos, Quaternion.identity);
+	
+				}
+			}
 		}
 
+		
+
 		if(Time.time - timer > 1)
+		{
 			renderer.enabled=true;
-
+			isTouch=true;
+			collider.enabled=true;
+		}
 	}
-
+	
+	
+	
 	//when player hit enemy, enemy will destroy
+	
 
 	void OnTriggerEnter(Collider collide)
 	{
-		if(collide.gameObject.CompareTag("Enemy"))
-		
-			//Enemy.y = -3.0f;
-			
-		if (collide.gameObject.CompareTag("Bullet"))
+		if(collide.gameObject.CompareTag("Enemy") || collide.gameObject.CompareTag("Bullet") )
 		{
-			
-			
+			playerLives--;
+			renderer.enabled=false;
+			isTouch=false;
+			collider.enabled=false;
+			timer=Time.time;
 		}
-		
-		playerLives--;
-		renderer.enabled=false;
-		timer=Time.time;
+			
 		
 		if(playerLives == 0)
 		{
